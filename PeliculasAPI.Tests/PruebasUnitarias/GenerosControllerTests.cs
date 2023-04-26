31b941b26b613d7c2222b7fd997137d54114b8d7
Jorge <jorge.rivadeneira.cevallos@gmail.com>
2023-04-26 12:30:21 -0500

    Agregada la primera prueba unitaria

---
+++ PeliculasAPI.Tests/PruebasUnitarias/GenerosControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeliculasAPI.Controllers;
+using PeliculasAPI.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliculasAPI.Tests.PruebasUnitarias
+{
+    [TestClass]
+    public class GenerosControllerTests : BasePruebas
+    {
+        [TestMethod]
+        public async Task ObtenerTodosLosGeneros()
+        {
+            // Preparación
+            var nombreBD = Guid.NewGuid().ToString();
+            var contexto = ConstruirContext(nombreBD);
+            var mapper = ConfigurarAutoMapper();
+
+            contexto.Generos.Add(new Genero() { Nombre = "Género 1" });
+            contexto.Generos.Add(new Genero() { Nombre = "Género 2" });
+            await contexto.SaveChangesAsync();
+
+            var contexto2 = ConstruirContext(nombreBD);
+
+            // Prueba
+            var controller = new GenerosController(contexto2, mapper);
+            var respuesta = await controller.Get();
+
+            // Verificación
+            var generos = respuesta.Value;
+            Assert.AreEqual(2, generos.Count);
+        }
+    }
+}
